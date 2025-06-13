b4f9cffe7aca950e7e90d5696536a62cdfe96a5d
iam-vihaan <asathyavihaan@gmail.com>
2025-06-13 14:32:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ module "ecr" {
   source = "./modules/ecr"
 }
 
-module "security_group" {
-  source = "./modules/security_group"
+module "security-group" {
+  source = "./modules/security-group"
   name   = var.name
   vpc_id = module.vpc.vpc_id
 }
